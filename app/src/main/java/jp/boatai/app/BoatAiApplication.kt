9ab3eb2e296c0d0a9f3551eeb9be7eba809f3cc4
daisuke05221995-cpu <daisuke05221995@gmail.com>
2026-09-22 19:27:00 +0900

    Reset prediction and purchase history on app version changes

---
+++ app/src/main/java/jp/boatai/app/BoatAiApplication.kt
@@ -0,0 +1,10 @@
+package jp.boatai.app
+
+import android.app.Application
+
+class BoatAiApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        VersionDataResetManager(this).resetIfVersionChanged()
+    }
+}
